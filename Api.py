6243f3781ca13a223c81c438ac7be7ee31ed6be7
tonlls <tonlls1999@gmail.com>
2021-12-12 11:09:09 +0100

    m

--- Api.py
+++ Api.py
@@ -1,4 +1,4 @@
-#uvicorn Api.py:app --reload
+#uvicorn Api:app --reload
 
 
 import Login
@@ -15,7 +15,8 @@ import User
 # from pydantic import BaseModel
 import datetime
 from uuid import uuid4
-
+from google.cloud.firestore_v1.field_path import FieldPath
+# import firestore as f
 
 app = FastAPI()
 
@@ -55,7 +56,7 @@ def register(user:User.User):
 @app.post("/user/login")
 def login(login:Login.Login):
     user = db.collection(u'users').document(f'{login.email}').get()
-    if user.to_dict()['password'] == login.password:
+    if user.exists and user.to_dict()['password'] == login.password:
         tok=generate_token()
         KEYS[login.email] = tok
         return tok
@@ -67,6 +68,11 @@ def check_token(token):
         if token == KEYS[key]:
             return True
     return False
+def get_email_from_token(token):
+    for key in KEYS:
+        if token == KEYS[key]:
+            return key
+    return False
 def generate_token():
     return str(uuid4())
 def get_actual_time():
@@ -74,21 +80,35 @@ def get_actual_time():
 def save_emotion_data(user_email, emotion_data):
     db.collection(u'users').document(f'{user_email}').collection(u'emotions').document(get_actual_time()).set(emotion_data)
 
-@app.post("/user/send_message")
-def add_message(user_email, message,token):
+@app.post("/user/add_message")
+def add_message(message,token):
     if check_token(token):
-        db.collection(u'users').document(f'{user_email}').collection(u'messages').document(get_actual_time()).set(message)
+        user_email = get_email_from_token(token)
+        db.collection(u'users').document(f'{user_email}').collection(u'messages').document(generate_token()).set({'data':message})
+        return True
+    else:
+        return False
+@app.post("/user/edit_message")
+def edit_message(id,message,token):
+    if check_token(token):
+        user_email = get_email_from_token(token)
+        db.collection(u'users').document(f'{user_email}').collection(u'messages').document(id).update({'data':message})
+        return True
     else:
         return False
 @app.get("/user/get_messages")
-def get_messages(user_email,token):
+def get_messages(token):
     if check_token(token):
-        messages = db.collection(u'users').document(f'{user_email}').collection(u'messages').get()
-        return messages
+        user_email = get_email_from_token(token)
+        messages = db.collection(u'users').document(f'{user_email}').collection(u'messages').stream()
+        out={}
+        for doc in messages:
+            out[doc.id]=doc.to_dict()
+        return out
     else:
         return False
 
-@app.post("/user/edit_message")
+# @app.post("/user/edit_message")
 
 @app.post("/user/add_to_journal")
 def add_to_journal(email, data,token):
@@ -99,15 +119,17 @@ def add_to_journal(email, data,token):
         # return False
 
 @app.get("/user/get_journal")
-def get_journal(email,token):
-    # if check_token(token):
-    journal = db.collection(u'users').document(f'{email}').collection(u'journal').stream()
-    out={}
-    for doc in journal:
-        out[doc.id]=doc.to_dict()
-    return out
-    # else:
-        # return False
+def get_journal(token):
+    if check_token(token):
+        email=get_email_from_token(token)
+        # journal = db.collection(u'users').document(f'{email}').collection(u'journal').order_by(FieldPath.document_id(),direction=firestore.Query.DESCENDING).stream()
+        journal = db.collection(u'users').document(f'{email}').collection(u'journal').stream()
+        out={}
+        for doc in journal:
+            out[doc.id]=doc.to_dict()
+        return out
+    else:
+        return False
 
 @app.get("/user/destroy_token")
 def destroy_token(token):
